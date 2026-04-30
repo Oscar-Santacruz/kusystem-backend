56104610436080d4d766a0d957ae977af9f5d157
Jazmin Santacruz <jazmin05santacruz@gmail.com>
2026-04-30 11:20:07 -0300

    fix: disable New Relic when no license key is set

--- newrelic.cjs
+++ newrelic.cjs
@@ -3,6 +3,7 @@
 exports.config = {
   app_name: [process.env.NEW_RELIC_APP_NAME || 'kusystem-backend'],
   license_key: process.env.NEW_RELIC_LICENSE_KEY || '',
+  agent_enabled: !!process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     level: 'info',
   },
